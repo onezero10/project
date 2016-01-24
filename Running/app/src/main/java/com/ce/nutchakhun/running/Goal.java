ac84107beb10d9e756b97eaedfef945f635da5af
Thanapat191 <nuttsu_18259@live.com>
2016-01-24 21:26:11 +0700

    goalSetleaw

--- Running/app/src/main/java/com/ce/nutchakhun/running/Goal.java
+++ Running/app/src/main/java/com/ce/nutchakhun/running/Goal.java
@@ -2,8 +2,15 @@ package com.ce.nutchakhun.running;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class Goal extends AppCompatActivity {
 
@@ -11,6 +18,24 @@ public class Goal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal);
+
+        Spinner actlevel = (Spinner) findViewById(R.id.spinner_level);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.array_actlevel, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        actlevel.setAdapter(adapter);
+
+        Spinner goal = (Spinner) findViewById(R.id.spinner_goal);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.array_goal, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        goal.setAdapter(adapter2);
     }
 
     @Override
@@ -34,4 +59,44 @@ public class Goal extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private boolean checkdata(){
+
+
+    return true;
+    }
+
+    public void sumbit_data(View view) {
+        if(checkdata()==true){
+            EditText edit = (EditText) findViewById(R.id.fill_age);
+            int age = Integer.parseInt(edit.getText().toString());
+            edit = (EditText) findViewById(R.id.fill_weight);
+            int weight = Integer.parseInt(edit.getText().toString());
+            edit = (EditText) findViewById(R.id.fill_height);
+            int height = Integer.parseInt(edit.getText().toString());
+            RadioGroup radioGroup = (RadioGroup) findViewById(R.id.sex);
+            int sex = radioGroup.getCheckedRadioButtonId();
+            if(sex==R.id.male) sex = 0;
+            else sex = 1;
+            Spinner spinner = (Spinner) findViewById(R.id.spinner_level);
+            int level = spinner.getSelectedItemPosition();
+            Spinner spinner2 = (Spinner) findViewById(R.id.spinner_goal);
+            int goal = spinner2.getSelectedItemPosition();
+            Log.d("age",age+"");
+            Log.d("weight",weight+"");
+            Log.d("Height",height+"");
+            Log.d("sex",sex+"");
+            Log.d("level",level+"");
+            Log.d("goal",goal+"");
+
+
+
+
+
+        }
+
+
+    }
+
+
 }
--- Running/app/src/main/java/com/ce/nutchakhun/running/MapsActivity.java
+++ Running/app/src/main/java/com/ce/nutchakhun/running/MapsActivity.java
@@ -1,86 +1,59 @@
 package com.ce.nutchakhun.running;
-
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 public class MapsActivity extends FragmentActivity {
-    GoogleMap mMap;
-    Marker mMarker;
-    LocationManager lm;
-    double lat, lng;
-
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        mMap = ((SupportMapFragment)getSupportFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
+        setUpMapIfNeeded();
     }
-
-    LocationListener listener = new LocationListener() {
-        public void onLocationChanged(Location loc) {
-            LatLng coordinate = new LatLng(loc.getLatitude()
-                    , loc.getLongitude());
-            lat = loc.getLatitude();
-            lng = loc.getLongitude();
-
-            if(mMarker != null)
-                mMarker.remove();
-
-            mMarker = mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(lat, lng)));
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                    coordinate, 16));
-        }
-
-        public void onStatusChanged(String provider, int status
-                , Bundle extras) {}
-        public void onProviderEnabled(String provider) {}
-        public void onProviderDisabled(String provider) {}
-    };
-
-    public void onResume() {
+    @Override
+    protected void onResume() {
         super.onResume();
-
-        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-
-        boolean isNetwork =
-                lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        boolean isGPS =
-                lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-        if(isNetwork) {
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, listener);
-            Location loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            if(loc != null) {
-                lat = loc.getLatitude();
-                lng = loc.getLongitude();
-            }
-        }
-
-        if(isGPS) {
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, listener);
-            Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if(loc != null) {
-                lat = loc.getLatitude();
-                lng = loc.getLongitude();
+        setUpMapIfNeeded();
+    }
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
             }
         }
     }
 
-    public void onPause() {
-        super.onPause();
-        lm.removeUpdates(listener);
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(13.730088, 100.775656)).title("Marker"));
     }
 }
+/**
+ * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+ * just add a marker near Africa.
+ * <p/>
+ * This should only be called once and when we are sure that {@link #mMap} is not null.
+ */
+/**
+ * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+ * installed) and the map has not already been instantiated.. This will ensure that we only ever
+ * call {@link #setUpMap()} once when {@link #mMap} is not null.
+ * <p/>
+ * If it isn't installed {@link SupportMapFragment} (and
+ * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+ * install/update the Google Play services APK on their device.
+ * <p/>
+ * A user can return to this FragmentActivity after following the prompt and correctly
+ * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+ * have been completely destroyed during this process (it is likely that it would only be
+ * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+ * method in {@link #onResume()} to guarantee that it will be called.
+ */
